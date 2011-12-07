168f71918af63ede536d8ed7ac4c340db9f2d61c
j.y.daddy.file@gmail.com <j.y.daddy.file@gmail.com@ec693368-41c4-fd5f-cca7-9ff289b81fc2>
2011-12-07 01:37:08 +0000

    storage status

---
+++ StorageStatus.java
@@ -0,0 +1,80 @@
+package com.j.y.daddy.F50RG;
+
+import java.io.File;
+
+import android.os.Environment;
+import android.os.StatFs;
+
+public class StorageStatus {
+
+        static final int ERROR = -1 ;
+        
+        public boolean externalMemoryAvailable() {
+            return android.os.Environment.getExternalStorageState().equals(android.os.Environment.MEDIA_MOUNTED);
+        }
+        
+        public long getAvailableInternalMemorySize() {
+                File path = Environment.getDataDirectory();
+                StatFs stat = new StatFs(path.getPath());
+                long blockSize = stat.getBlockSize();
+                long availableBlocks = stat.getAvailableBlocks();
+                return availableBlocks * blockSize;
+        }
+        
+        public long getTotalInternalMemorySize() {
+                File path = Environment.getDataDirectory();
+                StatFs stat = new StatFs(path.getPath());
+                long blockSize = stat.getBlockSize();
+                long totalBlocks = stat.getBlockCount();
+                return totalBlocks * blockSize;
+        }
+        
+        public long getAvailableExternalMemorySize() {
+                if(externalMemoryAvailable()) {
+                        File path = Environment.getExternalStorageDirectory();
+                        StatFs stat = new StatFs(path.getPath());
+                        long blockSize = stat.getBlockSize();
+                        long availableBlocks = stat.getAvailableBlocks();
+                        return availableBlocks * blockSize;
+                } else {
+                        return ERROR;
+                }
+        }
+        
+        public long getTotalExternalMemorySize() {
+                if(externalMemoryAvailable()) {
+                        File path = Environment.getExternalStorageDirectory();
+                        StatFs stat = new StatFs(path.getPath());
+                        long blockSize = stat.getBlockSize();
+                        long totalBlocks = stat.getBlockCount();
+                        return totalBlocks * blockSize;
+                } else {
+                        return ERROR;
+                }
+        }
+        
+        public String formatSize(long size) {
+                String suffix = null;
+        
+                if (size >= 1024) {
+                        suffix = "KiB";
+                        size /= 1024;
+                        if (size >= 1024) {
+                                suffix = "MiB";
+                                size /= 1024;
+                        }
+                }
+        
+                StringBuilder resultBuffer = new StringBuilder(Long.toString(size));
+        
+                int commaOffset = resultBuffer.length() - 3;
+                while (commaOffset > 0) {
+                        resultBuffer.insert(commaOffset, ',');
+                        commaOffset -= 3;
+                }
+        
+                if (suffix != null)
+                        resultBuffer.append(suffix);
+                return resultBuffer.toString();
+        }
+}
